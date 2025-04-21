cf30d11ca02ca6c4a5341b57ea1bc8a9fbd07995
oliveira-sh <0l1ve1r4@proton.me>
2025-04-20 22:20:26 -0300

    automated setup script

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e 
+set -o pipefail
+
+echo "[..] Creating virtual environment..."
+python3 -m venv .venv
+
+echo "[OK] Virtual environment created at .venv"
+
+source .venv/bin/activate
+
+echo "[..] Upgrading pip, setuptools, and wheel..."
+pip install --upgrade pip setuptools wheel
+
+echo "[..] Installing required Python packages..."
+pip install \
+    networkx \
+    maturin \
+    pandas \
+    matplotlib \
+    scikit-learn \
+    tqdm \
+    numpy \
+    python-louvain \
+    igraph \
+    leidenalg
+
+echo "[OK] Setup complete"
