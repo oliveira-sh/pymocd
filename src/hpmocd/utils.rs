3e9c26cf41ad94928e4459562a12a2c9c021735b
oliveira-sh <0l1ve1r4@proton.me>
2025-05-27 06:42:18 -0300

    docs: add GNU GPLv3 in missing files

--- src/hpmocd/utils.rs
+++ src/hpmocd/utils.rs
@@ -1,3 +1,7 @@
+//! This Source Code Form is subject to the terms of The GNU General Public License v3.0
+//! Copyright 2025 - Guilherme Santos. If a copy of the MPL was not distributed with this
+//! file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
+
 use crate::hpmocd::individual::Individual;
 
 use rustc_hash::FxHashMap as HashMap;
