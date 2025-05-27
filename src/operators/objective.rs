d05552c28001e930c9b55ee27a448f4514130a9e
oliveira-sh <0l1ve1r4@proton.me>
2025-05-27 06:42:18 -0300

    docs: add GNU GPLv3 in missing files

--- src/operators/objective.rs
+++ src/operators/objective.rs
@@ -1,4 +1,8 @@
 //! operators/objective.rs
+//! This Source Code Form is subject to the terms of The GNU General Public License v3.0
+//! Copyright 2025 - Guilherme Santos. If a copy of the MPL was not distributed with this
+//! file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
+
 use crate::graph::{Graph, NodeId, Partition};
 use crate::operators::metrics::Metrics;
 use rayon::iter::*;
