adda3096b45236a8006b033e6f3eaeafed449bc6
oliveira-sh <0l1ve1r4@proton.me>
2025-05-27 06:42:18 -0300

    docs: add GNU GPLv3 in missing files

--- src/hpmocd/individual.rs
+++ src/hpmocd/individual.rs
@@ -1,3 +1,8 @@
+//! hpmocd/individual.rs
+//! This Source Code Form is subject to the terms of The GNU General Public License v3.0
+//! Copyright 2025 - Guilherme Santos. If a copy of the MPL was not distributed with this
+//! file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
+
 use crate::graph::{Graph, Partition};
 use crate::operators;
 use rand::{prelude::*, rng};
