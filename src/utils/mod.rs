3cead20286d4125ffecab47666bb4a9bcdd26e02
oliveira-sh <0l1ve1r4@proton.me>
2025-05-27 06:36:54 -0300

    removed: unused files and algorithm, maked things cleaner

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,6 @@
 //! utils/mod.rs
 //! This Source Code Form is subject to the terms of The GNU General Public License v3.0
-//! Copyright 2024 - Guilherme Santos. If a copy of the MPL was not distributed with this
+//! Copyright 2025 - Guilherme Santos. If a copy of the MPL was not distributed with this
 //! file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
 
 use crate::graph::*;
