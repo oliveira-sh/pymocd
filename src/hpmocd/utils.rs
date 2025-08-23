9a89276e629a45771cc2db2ead9b31721d5ba306
oliveira-sh <oliveira.sec@proton.me>
2025-08-23 13:43:10 -0300

    changes: removed unused code, outdated functions.

--- src/hpmocd/utils.rs
+++ src/hpmocd/utils.rs
@@ -1,3 +1,4 @@
+//! hpmocd/utils.rs
 //! This Source Code Form is subject to the terms of The GNU General Public License v3.0
 //! Copyright 2025 - Guilherme Santos. If a copy of the MPL was not distributed with this
 //! file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
