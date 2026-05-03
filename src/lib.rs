7651344e506437381bff42142edc8c248074f751
Guilherme Oliveira <gui.olvr@proton.me>
2026-05-03 20:35:51 -0300

    refactor(lib): wire core module and stub_info gatherer

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,13 @@
 //! file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
 // ================================================================================================
 use pyo3::prelude::*;
-mod graph;
-mod hpmocd;
-mod macros;
-mod mocd; // deprecated
-mod operators;
-mod prism;
-mod utils;
-mod xfeats;
+mod api;
+pub mod core;
 // ================================================================================================
-use hpmocd::HpMocd;
-use prism::Prism;
-use xfeats::{fitness, set_thread_count};
+use api::{hpmocd_fn, prism_fn};
+use core::hpmocd::HpMocd;
+use core::prism::Prism;
+use core::xfeats::{fitness, set_thread_count};
 // ================================================================================================
 
 /// pymocd is a Python library, powered by a Rust backend, for performing efficient community
@@ -26,7 +21,11 @@ use xfeats::{fitness, set_thread_count};
 fn pymocd(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(set_thread_count, m)?)?;
     m.add_function(wrap_pyfunction!(fitness, m)?)?;
+    m.add_function(wrap_pyfunction!(prism_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(hpmocd_fn, m)?)?;
     m.add_class::<HpMocd>()?;
     m.add_class::<Prism>()?;
     Ok(())
 }
+
+pyo3_stub_gen::define_stub_info_gatherer!(stub_info);
