10352f18dbcac816aa3e4081778b00ca39753354
Guilherme Oliveira <gui.olvr@proton.me>
2026-06-28 21:07:50 -0300

    api: expose ariadne, hpmocd, mocd_q/d, moga_net, ccm, krm

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,15 @@
 //! This Source Code Form is subject to the terms of The GNU General Public License v3.0
 //! Copyright 2025 - Guilherme Santos. If a copy of the MPL was not distributed with this
 //! file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
-// ================================================================================================
 use pyo3::prelude::*;
 mod api;
 pub mod core;
-// ================================================================================================
-use api::{hpmocd_fn, prism_fn};
-use core::hpmocd::HpMocd;
-use core::prism::Prism;
+use api::{
+    ariadne_fn, ariadne_fronts_fn, ccm_fn, hpmocd_fn, krm_fn, moga_net_fn, mocd_d_fn, mocd_q_fn,
+};
+use core::algorithms::hpmocd::HpMocd;
+use core::algorithms::mocd::Mocd;
 use core::xfeats::{fitness, set_thread_count};
-// ================================================================================================
 
 /// pymocd is a Python library, powered by a Rust backend, for performing efficient community
 /// detection in complex networks.
@@ -21,10 +20,16 @@ use core::xfeats::{fitness, set_thread_count};
 fn pymocd(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(set_thread_count, m)?)?;
     m.add_function(wrap_pyfunction!(fitness, m)?)?;
-    m.add_function(wrap_pyfunction!(prism_fn, m)?)?;
     m.add_function(wrap_pyfunction!(hpmocd_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(ariadne_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(ariadne_fronts_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(mocd_q_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(mocd_d_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(moga_net_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(ccm_fn, m)?)?;
+    m.add_function(wrap_pyfunction!(krm_fn, m)?)?;
     m.add_class::<HpMocd>()?;
-    m.add_class::<Prism>()?;
+    m.add_class::<Mocd>()?;
     Ok(())
 }
 
